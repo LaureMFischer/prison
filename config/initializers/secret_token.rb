53b6b819c8e2dde07b652945c60e348c508e2c03
Laure Fischer <laurefischer27@gmail.com>
2014-03-13 15:30:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prison::Application.config.secret_key_base = '0116896595686d6dabe22071524071d7f45e31f9265f825abefe6be44ac2ffee370fc7477010f4d761d1bc9ee48d462f394d450909cb8299732b8b6707a035ce'
